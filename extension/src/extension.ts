45aa9f9c7e2d82c4bc77ce81528e82bd89eff4a7
Sup#2.0 <sup2point0@gmail.com>
2025-11-29 21:47:18 +0000

    feat - implement DualShift

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -3,19 +3,18 @@ import * as vs from "vscode";
 import { decorate } from "./decorate";
 
 
-export function activate(context: vs.ExtensionContext)
+export function activate(ctx: vs.ExtensionContext)
 {
 	console.log("kebab-caser is live!");
 
   vs.window.onDidChangeTextEditorSelection(e => {
-    if (!vs.window.activeTextEditor) return;
-    // if (e.document !== vs.window.activeTextEditor.document) return;
-
-    decorate(vs.window.activeTextEditor);
+    if (vs.window.activeTextEditor) {
+      decorate(vs.window.activeTextEditor);
+    }
   });
 }
 
 export function deactivate()
 {
-  console.log("kebab-caser disabled.")
+  console.log("kebab-caser disabled.");
 }

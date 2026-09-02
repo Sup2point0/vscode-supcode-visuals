cb46c853afc798a671acab8018edb2441a1ed08c
Sup#2.0 <sup2point0@gmail.com>
2026-09-02 11:41:51 +0100

    add playground test file

---
+++ extension/tests/playground.ts
@@ -0,0 +1,59 @@
+// kebab-casify
+let   an_ident
+let  _an_ident
+let   an_ident_
+let  _an_ident_
+let __an_ident
+let   an_ident__
+let __an_ident__
+
+let ident_1
+let ident_10
+let ident_100_is_long
+
+  (an_ident)   ;
+ (_an_ident)   ;
+  (an_ident_)  ;
+ (_an_ident_)  ;
+(__an_ident)   ;
+  (an_ident__) ;
+(__an_ident__) ;
+
+let an__ident
+
+let _
+let __
+let ___
+
+let _p
+let q_
+let _r_
+let s_t
+
+let t_0
+let t_1
+let t_9
+
+let obj: Record<string, any> = {};
+
+obj.field
+obj._field
+obj.field_
+obj._field_
+
+obj.__field
+obj.field__
+obj.__field__
+
+obj.a_field
+
+
+// DualShift
+1 + 2
+3 * 4
+5 + 6 * 7
+
+function a_func(a_param = null, another_param = 5)
+{
+	return 
+}

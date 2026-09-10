a21847b5efb7fbe48e34b61c0f58732afedb26d7
Sup#2.0 <sup2point0@gmail.com>
2026-09-10 13:57:28 +0100

    【ctx】add comment types

--- extension/src/constants.ts
+++ extension/src/constants.ts
@@ -4,17 +4,23 @@ type Lookaround = [
 	string | null
 ];
 
-const COMMENT_STYLES: Record<string, Lookaround> = {
+export const COMMENT_STYLES: Record<string, Lookaround> = {
 	SLASH: [null, "/", "/"],
 	HASH:  [null, "#", null]
 };
 
 export const COMMENT_SINGLE: Record<string, Lookaround> = {
 	c:          COMMENT_STYLES.SLASH,
+	cpp:        COMMENT_STYLES.SLASH,
+	csharp:     COMMENT_STYLES.SLASH,
+	css:        COMMENT_STYLES.SLASH,
 	haskell:    [null, "-", "-"],
+	ignore:     COMMENT_STYLES.HASH,
 	javascript: COMMENT_STYLES.SLASH,
 	typescript: COMMENT_STYLES.SLASH,
 	python:     COMMENT_STYLES.HASH,
 	ruby:       COMMENT_STYLES.HASH,
 	rust:       COMMENT_STYLES.SLASH,
+	scss:       COMMENT_STYLES.SLASH,
+	yaml:       COMMENT_STYLES.HASH,
 };
